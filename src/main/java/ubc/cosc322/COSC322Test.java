3ba4c9dfc69698018c9e270cafe665b5ab1c9abe
JacobVanzella <jacobvanzella@gmail.com>
2021-05-22 15:16:15 -0700

    Update COSC322Test.java

--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -1,9 +1,9 @@
-
 package ubc.cosc322;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import sfs2x.client.entities.Room;
 import ygraph.ai.smartfox.games.BaseGameGUI;
@@ -23,6 +23,8 @@ public class COSC322Test extends GamePlayer {
 
 	private String userName = null;
 	private String passwd = null;
+	private int player = 0;
+	private int opponent = 0;
 
 	// The main method
 	// @param args for name and passwd (current, any string would work)
@@ -52,7 +54,8 @@ public class COSC322Test extends GamePlayer {
 
 	@Override
 	public void onLogin() {
-		System.out.println("Congratualations!!! The server has indicated that you successfully logged in.\n");
+		System.out.println(
+				"Congratualations!!! " + "I am called because the server indicated that the login is successfully");
 
 		List<Room> rooms = this.gameClient.getRoomList();
 		for (Room room : rooms) {
@@ -74,7 +77,8 @@ public class COSC322Test extends GamePlayer {
 		ArrayList<Integer> gameState, queenPosCurr, queenPosNext, arrowPos;
 		String playerBlack, playerWhite;
 		gameState = this.gameState;
-
+		System.out.println("Message Type: " + messageType);
+		
 		// Catches failure to handle message and returns false with error printed to
 		// console
 		try {
@@ -89,6 +93,48 @@ public class COSC322Test extends GamePlayer {
 
 				// Update GUI board with new state after move
 				this.gamegui.updateGameState(queenPosCurr, queenPosNext, arrowPos);
+				
+				// Creates local game board that has correct size (index 0->9 x 0->9, i.e 10x10 board)
+				ArrayList<Integer> gameBoard = new ArrayList<Integer>();
+				int[] arrayBoard = new int[100];
+				for (int i = 0; i < gameState.size(); i++) {
+					if( i % 11 != 0 && i > 11 ) {
+						gameBoard.add(gameState.get(i));
+					}
+				}
+				for( int i = 0; i < gameBoard.size(); i ++) {
+					arrayBoard[i] = gameBoard.get(i);
+				}
+				RecursiveAI ai = new RecursiveAI(arrayBoard);
+				List<List<Integer>> moves = ai.fetchPlays(ai, player);
+				
+				int randomIndex = (int)( Math.random() * moves.size());
+				List<Integer> randomMove = moves.get(randomIndex);
+				int prevRow = randomMove.get(0);
+				int prevCol = randomMove.get(1);
+				int nextRow = randomMove.get(2);
+				int nextCol = randomMove.get(3);
+				int spearRow = randomMove.get(4);
+				int spearCol = randomMove.get(5);
+				int prevRowAdjusted = 9 - prevRow + 1;
+				int prevColAdjusted = prevCol + 1; 
+				int nextRowAdjusted = 9 - nextRow + 1;
+				int nextColAdjusted = nextCol + 1;
+				int spearRowAdjusted = 9 - spearRow + 1;
+				int spearColAdjusted = spearCol + 1;
+				ArrayList<Integer> queenToMove = new ArrayList<Integer>();
+				queenToMove.add(prevRowAdjusted);
+				queenToMove.add(prevColAdjusted);
+				ArrayList<Integer> queenGoesTo = new ArrayList<Integer>();
+				queenGoesTo.add(nextRowAdjusted);
+				queenGoesTo.add(nextColAdjusted);
+				ArrayList<Integer> spearGoesTo = new ArrayList<Integer>();
+				spearGoesTo.add(spearRowAdjusted);
+				spearGoesTo.add(spearColAdjusted);
+				System.out.println(queenToMove + " " + queenGoesTo + " " + spearGoesTo);
+				this.gameClient.sendMoveMessage(queenToMove, queenGoesTo, spearGoesTo);
+				this.gamegui.updateGameState(queenToMove, queenGoesTo, spearGoesTo);
+				
 				break;
 
 			case GameMessage.GAME_ACTION_START:
@@ -96,7 +142,14 @@ public class COSC322Test extends GamePlayer {
 				// msgDetails.get(AmazonsGameMessage.GAME_STATE);
 				playerBlack = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
 				playerWhite = (String) msgDetails.get(AmazonsGameMessage.PLAYER_WHITE);
-
+				gameState = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE);
+				if ( playerBlack == userName) {
+					player = 1;
+					opponent = 2;
+				}else {
+					player = 2;
+					opponent = 1;
+				}
 				System.out.println("\nGame Started!!\n" + playerBlack + "(B) vs. " + playerWhite + "(W)");
 				System.out.print("Board State: ");
 				for (int i = 11; i < gameState.size(); i++) {
@@ -129,9 +182,9 @@ public class COSC322Test extends GamePlayer {
 			System.err.println(e.toString());
 			System.err.println("\nERROR: Message Handling Failed\nmessageType: " + messageType + "\nmsgDetails: "
 					+ msgDetails.toString());
+			//throw(e);
 			return false;
 		}
-
 		return true;
 	}
 
