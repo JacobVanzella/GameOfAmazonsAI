45ad9612c6e3f20fc9799c4386690b3e091acc48
JacobVanzella <jacobvanzella@gmail.com>
2021-06-01 19:28:00 -0700

    Slight Speedup
    Improved speed modestly by optimizing getMoves

--- src/main/java/amazonsChess/RecursiveAI.java
+++ src/main/java/amazonsChess/RecursiveAI.java
@@ -38,108 +38,7 @@ public class RecursiveAI extends Board {
 
 		return score;
 	}
-/*
-	public List<List<Integer>> getMoves(RecursiveAI currBoard, int player) {
-		// The nested List has form (prevRow, prevCol, nextRow, nextCol) for available
-		// moves
-		List<List<Integer>> moves = new ArrayList<List<Integer>>(); // will store moves in 2D list
-		List<List<Integer>> queenPos = new ArrayList<List<Integer>>();
 
-		// System.out.println(currBoard.toString());
-
-		for (int row = 0; row < 10; row++) {
-			for (int col = 0; col < 10; col++) {
-				if (currBoard.getTile(row, col) == player) {
-					// System.out.println("Row: " + row + " Col: " + col);
-					ArrayList<Integer> queenN = new ArrayList<Integer>();
-					queenN.add(row);
-					queenN.add(col);
-					queenPos.add(queenN);
-				}
-			}
-		}
-		// Find possible moves for each queen
-		for (List<Integer> queenCordinates : queenPos) {
-			int currRow = queenCordinates.get(0);
-			int currCol = queenCordinates.get(1);
-			int nextRow = currRow;
-			int nextCol = currCol;
-			int dist = 0;
-			boolean canMove = true;
-			boolean spearCanMove = true;
-			int spearRow = -1;
-			int spearCol = -1;
-			int spearDist = 0;
-
-			for (int i = -1; i < 2; i++) {
-				for (int j = -1; j < 2; j++) {
-					// System.out.println(currBoard.toString());
-					canMove = true;
-					dist = 1;
-					try {
-						while (canMove) {
-							nextRow = currRow + i * dist;
-							nextCol = currCol + j * dist;
-
-							if (currBoard.getTile(nextRow, nextCol) == EMPTY) {
-								// System.out.println(currRow + " " + currCol + " " + nextRow + " " + nextCol);
-								int[][] currBoardArray = new int[10][10];
-								for (int ii = 0; ii < 10; ii++) {
-									for (int jj = 0; jj < 10; jj++) {
-										currBoardArray[jj][ii] = currBoard.getBoard()[jj][ii];
-									}
-								}
-								RecursiveAI testboard = new RecursiveAI(currBoardArray);
-								testboard.moveQueen(currRow, currCol, nextRow, nextCol, player);
-
-								for (int k = -1; k < 2; k++) {
-									for (int r = -1; r < 2; r++) {
-										spearCanMove = true;
-										spearDist = 1;
-										// spearRow = nextRow;
-										// spearCol = nextCol;
-										try {
-											while (spearCanMove) {
-												spearRow = nextRow + k * spearDist;
-												spearCol = nextCol + r * spearDist;
-												// System.out.println(currRow + " " + currCol + " " + nextRow + " " +
-												// nextCol);
-												// System.out.println(spearRow + " " + spearCol);
-												if (testboard.getTile(spearRow, spearCol) == EMPTY) {
-													// System.out.println(testboard.toString());
-													ArrayList<Integer> move = new ArrayList<Integer>();
-													move.add(currRow);
-													move.add(currCol);
-													move.add(nextRow);
-													move.add(nextCol);
-													move.add(spearRow);
-													move.add(spearCol);
-													//System.out.println(move);
-													moves.add(move);
-													spearDist++;
-												} else {
-													spearCanMove = false;
-												}
-											}
-										} catch (Exception e) {
-											spearCanMove = false;
-										}
-									}
-								}
-								dist++;
-							} else {
-								canMove = false;
-							}
-						}
-					} catch (Exception e) {
-						canMove = false;
-					}
-				}
-			}
-		}
-		return moves;
-	}
-	*/
 
 	public List<int[]> getMovesArray(RecursiveAI currBoard, int player) {
 		// The nested List has form (prevRow, prevCol, nextRow, nextCol) for available
@@ -188,13 +87,7 @@ public class RecursiveAI extends Board {
 
 							if (currBoard.getTile(nextRow, nextCol) == EMPTY) {
 								// System.out.println(currRow + " " + currCol + " " + nextRow + " " + nextCol);
-								int[][] currBoardArray = new int[10][10];
-								for (int ii = 0; ii < 10; ii++) {
-									for (int jj = 0; jj < 10; jj++) {
-										currBoardArray[jj][ii] = currBoard.getBoard()[jj][ii];
-									}
-								}
-								RecursiveAI testboard = new RecursiveAI(currBoardArray);
+								RecursiveAI testboard = new RecursiveAI(currBoard.getBoard());
 								testboard.moveQueen(currRow, currCol, nextRow, nextCol, player);
 
 								for (int k = -1; k < 2; k++) {
